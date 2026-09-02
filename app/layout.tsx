65945e57ebfb0e59c1f6120e4f967753ec2b7298
weldingworkforcealliance-sys <weldingworkforcealliance@gmail.com>
2026-09-02 10:52:22 -0400

    Show assigned instructor at top of teacher workspaces

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import type { ReactNode } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import TeacherIdentityBar from './teacher-identity-bar';
 import './styles.css';
 import './agenda/agenda.css';
 
@@ -64,6 +65,7 @@ export default function RootLayout({
               </Link>
             </nav>
           )}
+          <TeacherIdentityBar pathname={pathname} />
           {children}
         </div>
       </body>
