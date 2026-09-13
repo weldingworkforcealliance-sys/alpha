29d1643e694386024652ad621b19266f09cfe7e4
weldingworkforcealliance-sys <weldingworkforcealliance@gmail.com>
2026-09-13 12:59:33 -0400

    Trigger production deploy for working demo Live Classroom

--- app/demo/welding/page.tsx
+++ app/demo/welding/page.tsx
@@ -1,6 +1,7 @@
 import WeldingDemoLiveWorkspace from './WeldingDemoLiveWorkspace';
 import { WELDING_DEMO_PROGRAM } from '../_data/welding-program';
 
+// Public welding demo entrypoint. Live Classroom remains isolated from live school records.
 export default function WeldingDemoPage() {
   return <WeldingDemoLiveWorkspace program={WELDING_DEMO_PROGRAM} />;
 }
