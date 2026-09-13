c02956aeed554e34a9998808df71f88840ca10bf
weldingworkforcealliance-sys <weldingworkforcealliance@gmail.com>
2026-09-13 12:48:55 -0400

    Wire welding demo to functional Live Classroom workspace

--- app/demo/welding/page.tsx
+++ app/demo/welding/page.tsx
@@ -1,6 +1,6 @@
-import DemoProgramWorkspace from '../_components/DemoProgramWorkspace';
+import WeldingDemoLiveWorkspace from './WeldingDemoLiveWorkspace';
 import { WELDING_DEMO_PROGRAM } from '../_data/welding-program';
 
 export default function WeldingDemoPage() {
-  return <DemoProgramWorkspace program={WELDING_DEMO_PROGRAM} />;
+  return <WeldingDemoLiveWorkspace program={WELDING_DEMO_PROGRAM} />;
 }
