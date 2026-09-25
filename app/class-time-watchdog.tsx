b9f74017523203581ac028676b11a22360202ee1
weldingworkforcealliance-sys <weldingworkforcealliance@gmail.com>
2026-09-25 14:43:29 -0400

    ui: label watchdog action with affected course

--- app/class-time-watchdog.tsx
+++ app/class-time-watchdog.tsx
@@ -330,7 +330,7 @@ export default function ClassTimeWatchdog() {
                     fontWeight: 800,
                   }}
                 >
-                  Open Class
+                  {alert.courseLabel ? `Open ${alert.courseLabel}` : 'Open Class'}
                 </a>
                 {alert.severity !== 'start' && (
                   <a
